fc9cb98b9ccb5ba695fbfdb27902b89da5f95a69
Petsyy <peterarenasdiaz16@gmail.com>
2026-08-18 13:54:35 +0800

    feat: remove the config.transformer

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -10,10 +10,4 @@ config.resolver.alias = {
   "@": path.resolve(__dirname, "."),
 };
 
-// Ensure Hermes-incompatible syntax (like private class fields) is transpiled
-config.transformer = {
-  ...config.transformer,
-  unstable_transformProfile: "hermes-stable",
-};
-
 module.exports = withNativeWind(config, { input: "./global.css" });
